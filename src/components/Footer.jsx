a631123caf43f3a9b938248d655255e6ab37e0f3
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-07-01 13:39:59 +0530

    Add : simple _react_app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>Krishna</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.youtube.com/channel/UCvrC1t_1LgkLdKNIC7hQK2w" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://www.instagram.com/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/Krishanaku?tab=repositories" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
